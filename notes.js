ba843a1acf60336e9c8d4b4124ae2ad1aed438df
Richard Brooks <rickbrooks22@gmail.com>
2018-06-15 08:21:40 -0400

    added error validation on duplicate titles

--- notes.js
+++ notes.js
@@ -1,7 +1,26 @@
 console.log('Starting notes.js');
+const fs = require('fs');
 
 const addNote = (title, body) => {
-    console.log('Adding note', title, body);
+    var notes = [];
+    var note = {
+        title, 
+        body
+    };
+
+    try {
+        var notesString = fs.readFileSync('notes-data.json');
+        notes = JSON.parse(notesString);
+    } catch (err) {
+
+    }
+
+    var duplicateNotes  = notes.filter((note) => note.title === title);
+
+    if(duplicateNotes.length === 0) {
+        notes.push(note);
+        fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+    };
 };
 
 const getAll = () => {
